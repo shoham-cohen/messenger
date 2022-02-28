80d420118d11ec29bb0dbb29b0358edea1347853
shoham-cohen <93212774+shoham-cohen@users.noreply.github.com>
2022-02-28 13:38:40 +0200

    private messages added

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import threading
 HOST = '127.0.0.1'
 PORT = 55000
 
+# creating the server socket and binds it
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
@@ -13,11 +14,15 @@ clients = []
 nicknames = []
 
 
-def broadcatst(message):
+# send the message to the wanted clients
+def broadcatst(message, To):
     for client in clients:
-        client.send(message)
+        index = clients.index(client)
+        if nicknames[index] in To:
+            client.send(message)
 
 
+# listening to the client and handling it's requests
 def handle(client):
     while True:
         try:
@@ -25,22 +30,28 @@ def handle(client):
             m = message.decode('utf-8')
             index = clients.index(client)
             nickname = nicknames[index]
-            if m == f"{nickname}: who is connected?\n":
+            if "Private:" in m:
+                To = list(m.split(" "))
+                message = client.recv(1024)
+                broadcatst(message, To[1:])
+                print(f"#Private# To: {To[1:-1]} From: {message.decode('utf-8')}", end=" ")
+            elif m == f"{nickname}: who is connected?\n":
                 client.send(f"Online: {str(nicknames)}".encode('utf-8'))
             else:
                 print(f"{m}", end=" ")
-                broadcatst(message)
+                broadcatst(message, nicknames)
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcatst(f"{nickname} is now offline".encode('utf-8'))
+            broadcatst(f"{nickname} is now offline\n".encode('utf-8'), nicknames)
             print(f"{nickname} is now offline")
             nicknames.remove(nickname)
             break
 
 
+# receives new clients and starts the handle method
 def receive():
     while True:
         client, address = server.accept()
@@ -52,11 +63,11 @@ def receive():
         clients.append(client)
 
         print(f"Nickname of the client is {nickname}")
-        broadcatst(f"{nickname} connected to the server!\n".encode('utf-8'))
-        #client.send("you are connected to the server\n".encode('utf-8'))
+        broadcatst(f"{nickname} connected to the server!\n".encode('utf-8'), nicknames)
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+        # client.send("you are connected to the server\n".encode('utf-8'))
 
 
 print("Server is running...")
